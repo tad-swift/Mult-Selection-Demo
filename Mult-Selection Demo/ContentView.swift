ec896b42791160ebd23c93f82c3fc6994edd38f6
Tadreik Campbell <tadreik@gmail.com>
2023-07-29 04:48:39 -0400

    initial commit

--- Mult-Selection Demo/ContentView.swift
+++ Mult-Selection Demo/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var options: [String] = ["Option 1", "Option 2", "Option 3"]
+    @State var selectedOptions: [String] = []
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ForEach(options, id: \.self) { option in
+                MyButton(arrayToAddValueTo: $selectedOptions, value: option)
+            }
         }
         .padding()
+    }
+}
+
+struct MyButton: View {
+    @Binding var arrayToAddValueTo: [String]
+    @State private var isSelected = false
+    var value: String
+    
+    var body: some View {
+        Text(value)
+            .padding()
+            .background {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(isSelected ? .green : .green.opacity(0.5))
+            }
+            .onTapGesture {
+                isSelected.toggle()
+                if isSelected {
+                    arrayToAddValueTo.append(value)
+                } else {
+                    if let index = arrayToAddValueTo.firstIndex(of: value) {
+                        arrayToAddValueTo.remove(at: index)
+                    }
+                }
+                print(arrayToAddValueTo)
+            }
     }
 }
 
