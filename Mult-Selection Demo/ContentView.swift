0bfae722a0c136d78139380b59ecfbdb817c617d
Tadreik Campbell <tadreik@gmail.com>
2023-07-29 04:39:31 -0400

    Initial Commit

---
+++ Mult-Selection Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mult-Selection Demo
+//
+//  Created by Tadreik Campbell on 7/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mult-Selection Demo/Mult_Selection_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mult_Selection_DemoApp.swift
+//  Mult-Selection Demo
+//
+//  Created by Tadreik Campbell on 7/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Mult_Selection_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
